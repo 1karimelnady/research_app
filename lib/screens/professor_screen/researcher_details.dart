fc8bf44eda403c50cb86f6f7f153ecbaf22ea692
mostafa-elnady <mostafa.elnady49@gmail.com>
2024-03-03 08:58:40 +0200

    before feedback

---
+++ lib/screens/professor_screen/researcher_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ResearcherDetailScreen extends StatelessWidget {
+  final String id;
+  const ResearcherDetailScreen({Key? key, required this.id}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
